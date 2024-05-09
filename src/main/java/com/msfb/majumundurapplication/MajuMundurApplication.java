73e333ba054792b47e5c3fff8085a6773fde99e5
shodiqbasiru <shodiqbasiru@gmail.com>
2024-05-09 19:53:00 +0700

    Initial commit

---
+++ src/main/java/com/msfb/majumundurapplication/MajuMundurApplication.java
@@ -0,0 +1,13 @@
+package com.msfb.majumundurapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajuMundurApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajuMundurApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/msfb/majumundurapplication/MajuMundurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.msfb.majumundurapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MajuMundurApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
